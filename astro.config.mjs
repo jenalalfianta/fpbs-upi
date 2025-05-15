36b9be7040752f5cb15a2aabaa6314a490a8d11e
Jenal Alfianta <jenalalfianta@gmail.com>
2025-05-15 08:53:01 +0700

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://fpbs.upi.edu', 
   vite: {
-    plugins: [tailwindcss( 
-    )]
+    plugins: [tailwindcss()]
   },
-
   output: 'static',
-  build: { format: 'file' }
-
+  build: { format: 'file' },
+  integrations: [sitemap()]
 });
-
-
