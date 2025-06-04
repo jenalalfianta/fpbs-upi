f1eca896761dbf5dcc9002cfe864de5236b63871
Jenal Alfianta <jenalalfianta@gmail.com>
2025-06-04 14:54:44 +0700

    perbaikan layout.astro (title untuk seo)

--- src/scripts/title.ts
+++ src/scripts/title.ts
@@ -1,6 +1,10 @@
 export function getPageTitle(pathname: string): string {
   if (pathname === "/") return "Fakultas Pendidikan Bahasa dan Sastra - Beranda";
-  const words = pathname.replace(/\//g, '').split('-');
-  const capitalized = words.map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ');
-  return `Fakultas Pendidikan Bahasa dan Sastra - ${capitalized}`;
+
+  const parts = pathname.split('/').filter(Boolean); // buang '' kosong
+  const lastSegment = parts[parts.length - 1]; // ambil bagian terakhir path
+  const words = lastSegment.split('-').map(w => w.charAt(0).toUpperCase() + w.slice(1));
+  const label = words.join(' ');
+
+  return `Fakultas Pendidikan Bahasa dan Sastra - ${label}`;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      // Tambahkan path ke semua file yang menggunakan class Tailwind
       './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     ],
     theme: {
       extend: {
-        // Anda bisa kustomisasi tema di sini jika perlu
       },
     },
     plugins: [
-      // Tambahkan plugin typography di sini
+      //plugin typography
       require('@tailwindcss/typography'),
     ],
   };
